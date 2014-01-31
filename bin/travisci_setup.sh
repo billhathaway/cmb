0f10eafe3400d37c6793477756d5e386dbc174d8
Bill Hathaway <william_hathaway@cable.comcast.com>
2014-01-31 12:03:07 -0800

    add debug statements to setup script

--- bin/travisci_setup.sh
+++ bin/travisci_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 mvn -Dmaven.test.skip=true assembly:assembly
+echo "Finished mvn"
 tar -xzvCf /tmp  target/cmb-distribution-*
+echo "Unpacking distribution in /tmp"
 (cd /tmp/cmb && nohup bin/cmb.sh &)
+echo "Ran cmb.sh"
+sleep 3
+ps aux | grep cmb
