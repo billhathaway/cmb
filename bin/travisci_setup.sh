004f4741009aae2cc16f600535c13abfaf4e309a
Bill Hathaway <william_hathaway@cable.comcast.com>
2014-01-31 13:30:56 -0800

    change log4j test settings to remove console output

--- bin/travisci_setup.sh
+++ bin/travisci_setup.sh
@@ -4,6 +4,7 @@ echo "*** Finished mvn"
 echo "*** Starting untar into tmp"
 tar -xvz -C /tmp -f target/cmb-distribution-*
 echo "*** Finished untar into tmp"
+cp /tmp/cmb/config/test.log4j.properties /tmp/cmb/config/log4j.properties
 (cd /tmp/cmb && nohup bin/cmb.sh &)
 echo "*** Ran cmb.sh"
 sleep 5
